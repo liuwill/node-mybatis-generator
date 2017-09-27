910fd28f5ac72f24104cb22fc45c4cfb338b8f06
liuwill <liuwei_will@qq.com>
2017-09-27 11:49:59 +0800

    check if nunjucks work

---
+++ test/unit/nunjucks.spec.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var chai = require('chai')
+var nunjucks = require("nunjucks")
+
+chai.should()
+var expect = chai.expect
+
+describe('#nunjucks features', function() {
+  it('is template works', function() {
+    var languages = ['Java', 'javascript', 'Python']
+    var nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader('examples'))
+    var result = nunjucksEnv.render('template.njk', { languages: languages })
+    expect(result).to.include(languages[0])
+  })
+})
