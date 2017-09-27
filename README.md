48e976763abf6e4d733ad18f91abd3423b89d0fb
liuwill <liuwei_will@qq.com>
2017-09-27 19:46:16 +0800

    FEATURE: table

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 使用node.js实现的mybatis generator，生成mybatis mapper的JAVA代码。
 
-
 ## License
 
   [MIT](./LICENSE)
---
+++ test/unit/loader.spec.js
@@ -0,0 +1,67 @@
+'use strict'
+
+var fs = require('fs')
+var path = require('path')
+var chai = require('chai')
+
+chai.should()
+var expect = chai.expect
+var assert = chai.assert
+
+var loader = require('../../lib/loader')
+
+describe('#file loader', function () {
+  var randPath = Math.random().toString(36).substr(2, 9)
+
+  describe('#load Json', function () {
+    it('will throw error when file not exists', function () {
+      expect(function () { loader.loadJson(path.join(__filename, './' + randPath + '/path')) }).to.throw()
+      expect(function () { loader.loadJson(__dirname) }).to.throw()
+      expect(function () { loader.loadJson(__filename) }).to.throw()
+    })
+
+    it('will load json content from file', function () {
+      var jsonData = {
+        "host": "localhost",
+        "user": "root",
+        "password": "123456",
+        "database": "demo",
+        "port": 3306
+      }
+
+      var fileName = 'json_' + randPath
+      var writePath = path.join(__dirname, fileName)
+      fs.writeFileSync(writePath, JSON.stringify(jsonData))
+
+      var readData = loader.loadJson(writePath)
+      expect(readData).to.deep.equal(jsonData)
+
+      fs.unlinkSync(writePath)
+    })
+  })
+
+  describe('#init target', function () {
+    it('will throw error where directory not exists', function () {
+      expect(function () { loader.initTarget(path.join(__filename, './' + randPath + '/path')) }).to.throw()
+    })
+
+    it('will throw error when directory is not empty', function () {
+      expect(function () { loader.initTarget('./') }).to.throw()
+    })
+
+    it('will throw error when file passed', function () {
+      expect(function () { loader.initTarget(__filename) }).to.throw()
+      expect(function () { loader.initTarget(path.join(__filename, './path')) }).to.throw()
+    })
+
+    it('will create directory', function () {
+      var filePath = './temp_' + randPath
+      assert.isFalse(fs.existsSync(filePath))
+
+      loader.initTarget(filePath)
+
+      assert.isTrue(fs.existsSync(filePath))
+      fs.rmdirSync(filePath)
+    })
+  })
+})
